c22391b3490e443db181044a245285f4d127afdf
surerever <nike-gri123@mail.ru>
2021-01-17 19:47:39 +0300

    Updates

--- precache-manifest.5e6f38b78daa1a3db44cc7493431441a.js
+++ precache-manifest.5e6f38b78daa1a3db44cc7493431441a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c5089e957f285c526880d09554cbacca",
+    "revision": "f3ccb1dffc59ec0a4e6b0c2fff7e261b",
     "url": "/CodeRegion/index.html"
   },
   {
-    "revision": "7d74bb761b9d45a09911",
+    "revision": "9ecba5274496e1f6f909",
     "url": "/CodeRegion/static/css/2.0ca46490.chunk.css"
   },
   {
-    "revision": "1598b0e67c5c864aa5f0",
+    "revision": "dc379110beafcc61154c",
     "url": "/CodeRegion/static/css/main.df66b65a.chunk.css"
   },
   {
-    "revision": "7d74bb761b9d45a09911",
-    "url": "/CodeRegion/static/js/2.acd28270.chunk.js"
+    "revision": "9ecba5274496e1f6f909",
+    "url": "/CodeRegion/static/js/2.d231ced9.chunk.js"
   },
   {
-    "revision": "1598b0e67c5c864aa5f0",
-    "url": "/CodeRegion/static/js/main.06652215.chunk.js"
+    "revision": "dc379110beafcc61154c",
+    "url": "/CodeRegion/static/js/main.6c8c9f0c.chunk.js"
   },
   {
     "revision": "036b10e6fc5e034fb95b",
